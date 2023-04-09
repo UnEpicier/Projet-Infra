56bbacf8c734c01f43266f6e4ade55d4feb45f30
GLAROUMANIE <gabriel.laroumanie@ynov.com>
2023-04-09 22:09:01 +0200

    + debut readme

--- start.sh
+++ start.sh
@@ -18,7 +18,7 @@ ssh $REVERSE_API_SSH_USER@$REVERSE_API_SSH_HOST -p $REVERSE_API_SSH_PORT 'bash -
 scp -P $DB_SSH_PORT ./db/mysql_installation.sql $DB_SSH_USER@$DB_SSH_HOST:/tmp/mysql_installation.sql
 ssh -p $DB_SSH_PORT $DB_SSH_USER@$DB_SSH_HOST < ./db/db.sh
 
-ssh -p $API_SSH_PORT $API_SSH_USER@$API_SSH_HOST "cd /home/api/MusicAPI && npm start"
+ssh -p $API_SSH_PORT $API_SSH_USER@$API_SSH_HOST 'bash -s' < "cd /home/api/MusicAPI && npm start"
 ssh $DB_SSH_USER@$DB_SSH_HOST -p $DB_SSH_PORT 'bash -s' < ./scripts/db.sh
 
 ssh $WEB_SSH_USER@$WEB_SSH_HOST -p $WEB_SSH_PORT 'bash -s' < ./web_script/web_script.sh
